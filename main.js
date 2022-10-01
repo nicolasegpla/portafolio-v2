9acee3977904893072710c67236958560ae38675
nicolasegpla <nicolasedixongarciapineros@gmail.com>
2022-09-30 23:34:07 -0500

    home terminado y funcional

---
+++ main.js
@@ -0,0 +1,33 @@
+const buttonDarkMode = document.querySelector('.container-button-dark-mode');
+const buttonSun = document.querySelector('.container-button-dark-mode__sun');
+const buttonMoon = document.querySelector('.container-button-dark-mode__moon');
+const backgroundBody = document.querySelector('body');
+const buttonMain = document.querySelector('.container-info__buttons__primary');
+const githubSun = document.querySelector('.github-icon');
+const githubMoon = document.querySelector('.github-dark');
+const profession = document.querySelector('.container-info__text__profession');
+const linkMain = document.querySelector('.container-info__buttons__link');
+const arrowDark = document.querySelector('.arrow-dark')
+const arrowDark2 = document.querySelector('.arrow-dark2')
+
+
+buttonDarkMode.addEventListener('click', toggleDarkMode);
+buttonMain.addEventListener('click', goToPage)
+
+function toggleDarkMode() {
+    buttonDarkMode.classList.toggle('container-second-color');
+    buttonSun.classList.toggle('inactive');
+    buttonMoon.classList.toggle('inactive');
+    backgroundBody.classList.toggle('background-body');
+    buttonMain.classList.toggle('button-dark-mode');
+    githubSun.classList.toggle('inactive');
+    githubMoon.classList.toggle('inactive');
+    profession.classList.toggle('text-color-dark');
+    linkMain.classList.toggle('text-color-dark');
+    arrowDark.classList.toggle('inactive');
+    arrowDark2.classList.toggle('inactive');
+}
+
+function goToPage(){
+    window.open ('https://github.com/nicolasegpla');
+}
